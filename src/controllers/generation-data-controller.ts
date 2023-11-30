50f0b205a6dd469511904edc62c6baf8e7561a93
Aaron Mineen <amineen64@hotmail.com>
2023-11-30 14:24:25 +0200

    Add routes for getting generation data by date and totalizer data

--- src/controllers/generation-data-controller.ts
+++ src/controllers/generation-data-controller.ts
@@ -1,6 +1,7 @@
 import { GenerationDataType } from "../models/types";
 import { GenerationDataSchemaModel } from "../models/GenerationDataSchema";
 import { Request, Response } from "express";
+import TotalizerDataModel from "../models/TotalizerDataModel";
 
 //@desc Insert array of generation data into database
 //@route POST /api/v1/generation-data
@@ -53,6 +54,177 @@ export const getLastGenerationDataReading = async (
     return res.status(200).json({
       success: true,
       data: result,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//@desc GET data for a given date. date in the format of YYYY-MM-DD.
+//@route GET /api/v1/generation-data/:date
+//@access Private
+export const getGenerationDataByDate = async (req: Request, res: Response) => {
+  try {
+    let date = req.query.date;
+    //if date is not provided, set date to today
+    if (!date) {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+      date = `${year}-${month}-${day}`;
+    }
+
+    const result = await GenerationDataSchemaModel.aggregate([
+      {
+        $match: {
+          timestamp: { $regex: date },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          load: { $sum: "$LOAD_E_total" },
+          genEnergy: { $sum: "$GEN_E_total" },
+          batteryDIS: { $sum: "$INV_E_total_DIS" },
+          batteryCHG: { $sum: "$INV_E_total_CHG" },
+          pvEnergy: { $sum: "$PV_E_total" },
+          peakLoad: { $max: "$LOAD1_P_total_max" },
+          records: {
+            $push: {
+              BMS1_P_total_current: "$BMS1_P_total_current",
+              BMS1_SOC: "$BMS1_SOC",
+              GEN_P_total_current: "$GEN_P_total_current",
+              INV1_P_total_current: "$INV1_P_total_current",
+              INV2_P_total_current: "$INV2_P_total_current",
+              LOAD1_P_total_current: "$LOAD1_P_total_current",
+              PV1_P_total_current: "$PV1_P_total_current",
+              PV2_P_total_current: "$PV2_P_total_current",
+              timestamp: "$timestamp",
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          load: 1,
+          genEnergy: 1,
+          batteryDIS: 1,
+          batteryCHG: 1,
+          pvEnergy: 1,
+          peakLoad: 1,
+          records: 1,
+        },
+      },
+    ]);
+    return res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//@desc GET totalizer daily readings for a given date. date in the format of YYYY-M-DD.
+//@route GET /api/v1/generation-data/daily-data?date=YYYY-MM-DD
+//@access Private
+
+// export const getTotalizerDailyReadings = async (
+//   req: Request,
+//   res: Response
+// ) => {
+//   try {
+//     let date = req.query.date;
+//     //if date is not provided, set date to today
+//     if (!date) {
+//       const today = new Date();
+//       const year = today.getFullYear();
+//       const month = today.getMonth() + 1;
+//       const day = today.getDate();
+//       date = `${year}-${month}-${day}`;
+//     }
+
+//     const result = await TotalizerDataModel.findOne({
+//       dateKey: date,
+//     });
+
+//     return res.status(200).json({
+//       success: true,
+//       data: result,
+//     });
+//   } catch (error: any) {
+//     return res.status(500).json({
+//       success: false,
+//       error: error.message,
+//     });
+//   }
+// };
+
+export const getTotalizerDailyReadings = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    let date = req.query.date;
+    //if date is not provided, set date to today
+    if (!date) {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+      date = `${year}-${month}-${day}`;
+    }
+
+    const result = await TotalizerDataModel.aggregate([
+      { $match: { dateKey: date } },
+      {
+        $unwind: "$readings",
+      },
+      {
+        $project: {
+          _id: 0,
+          dailyAccumulativeEnergy: 1,
+          dailykWh: 1,
+          "readings.L1_current_avg": "$readings.L1_current_avg",
+          "readings.L2_current_avg": "$readings.L2_current_avg",
+          "readings.L3_current_avg": "$readings.L3_current_avg",
+          "readings.total_current_avg": "$readings.total_current_avg",
+          "readings.total_true_power_avg": "$readings.total_true_power_avg",
+          "readings.datetime": "$readings.datetime",
+        },
+      },
+
+      {
+        $group: {
+          _id: null,
+          dailyAccumulativeEnergy: { $first: "$dailyAccumulativeEnergy" },
+          dailyKWh: { $first: "$dailykWh" },
+          peakLoad: { $max: "$readings.total_true_power_avg" },
+          readings: {
+            $push: {
+              L1_current_avg: "$readings.L1_current_avg",
+              L2_current_avg: "$readings.L2_current_avg",
+              L3_current_avg: "$readings.L3_current_avg",
+              total_current_avg: "$readings.total_current_avg",
+              total_true_power_avg: "$readings.total_true_power_avg",
+              datetime: "$readings.datetime",
+            },
+          },
+        },
+      },
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      data: result[0],
     });
   } catch (error: any) {
     return res.status(500).json({
--- src/routes/generation-data-routes.ts
+++ src/routes/generation-data-routes.ts
@@ -8,4 +8,12 @@ router
   .route("/last-reading")
   .get(generationDataController.getLastGenerationDataReading);
 
+router
+  .route("/daily-data")
+  .get(generationDataController.getGenerationDataByDate);
+
+router
+  .route("/totalizer-data")
+  .get(generationDataController.getTotalizerDailyReadings);
+
 export default router;
